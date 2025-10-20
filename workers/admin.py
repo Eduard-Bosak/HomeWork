a1f67da41a001a16b0a7f4f2dd93be1baa062cf6
Eduard Bosak <eduard.bosak@example.com>
2025-10-20 17:53:16 +0300

    Initial commit: Django HomeWork project with corrections and instructions

---
+++ workers/admin.py
@@ -0,0 +1,45 @@
+from tkinter import Image
+from django.contrib import admin
+from import_export import resources
+from .models import Person
+
+
+class PersonResource(resources.ModelResource):
+    class Meta:
+        model = Person
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'is_on_main', 'name', 'skills', 'skill_level', 'employment')
+    list_editable = ('skills', 'skill_level', 'is_on_main')
+    search_fields = ('id', 'name', 'sex', 'skills', 'skill_level', 'description', 'employment')
+    list_filter = ('name', 'skills',)
+    list_display_links = ('id', 'name',)
+
+    resources_classes = [PersonResource]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Register your models here.
